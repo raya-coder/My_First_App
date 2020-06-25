809a831e70120d6fd58958a6f3ff6c20825d397d
raya-coder <raya.lasmar@ensi-uma.tn>
2020-06-25 18:09:22 +0100

    add some code

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.app
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.setContentView(R.layout.activity_main)
+        "Hello From Android".toast(context)
+        fun Any.toast(context: Context, duration: Int = Toast.LENGTH_SHORT): Toast {
+            return Toast.makeText(context, this.toString(), duration).apply { show() }
+        }
+        
     }
 }
